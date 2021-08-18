21dd1952cb18c90aef887a0f235b62e514d98918
Vadim Vrublevsky <v.v.vrublevsky@gmail.com>
2021-08-18 20:03:54 +0300

    Added material-ui & theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+
+import { theme } from "./theme";
 
 const App: React.FC = () => {
-  return <div>Hello Market Place!</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div>hello</div>
+    </ThemeProvider>
+  );
 };
 
 export default App;
